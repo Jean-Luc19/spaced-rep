4c5f2571630181377127a142aeebc9d927f42a9e
Aaron Gottlieb <aticus19@gmail.com>
2017-04-17 09:24:41 -0600

    modularizes server directory

--- server/index.js
+++ server/index.js
@@ -5,13 +5,14 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const BearerStrategy = require('passport-http-bearer').Strategy;
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
+const routes = require('./routes/routes');
 const User = require('./models/user')
 
 let secret = {
   CLIENT_ID: process.env.CLIENT_ID,
   CLIENT_SECRET: process.env.CLIENT_SECRET
 }
-
+console.log(secret)
 if(process.env.NODE_ENV != 'production') {
   secret = require('./secret');
 }
@@ -24,94 +25,95 @@ const database = {
 
 app.use(passport.initialize());
 
-passport.use(
-    new GoogleStrategy({
-        clientID:  secret.CLIENT_ID,
-        clientSecret: secret.CLIENT_SECRET,
-        callbackURL: `/api/auth/google/callback`
-    },
-    (accessToken, refreshToken, profile, cb) => {
-        // Job 1: Set up Mongo/Mongoose, create a User model which store the
-        // google id, and the access token
-        // Job 2: Update this callback to either update or create the user
-        // so it contains the correct access token
+app.use('/', routes);
 
-        const searchQuery = {
-            googleId: profile.id
-        };
-
-        const user = database[accessToken] = {
-            googleId: profile.id,
-            accessToken: accessToken
-        };
-
-        const updates = {
-            name: profile.displayName,
-            accessToken: accessToken,
-            googleId: profile.id
-        };
-
-        const options = {
-            upsert: true
-        };
-
-        User.findOneAndUpdate(searchQuery, updates, options, (err, user) => {
-            if (err) {
-                return cb(err);
-            }
-            else {
-                return cb(null, user);
-            }
-        })
-
-    }
-));
-
-passport.use(
-    new BearerStrategy(
-        (token, done) => {
-            // Job 3: Update this callback to try to find a user with a
-            // matching access token.  If they exist, let em in, if not,
-            // don't.
-            if (!(token in database)) {
-                return done(null, false);
-            }
-            return done(null, database[token]);
-        }
-    )
-);
-
-app.get('/api/auth/google',
-    passport.authenticate('google', {scope: ['profile']}));
-
-app.get('/api/auth/google/callback',
-    passport.authenticate('google', {
-        failureRedirect: '/',
-        session: false
-    }),
-    (req, res) => {
-        res.cookie('accessToken', req.user.accessToken, {expires: 0});
-        res.redirect('/');
-    }
-);
-
-app.get('/api/auth/logout', (req, res) => {
-    req.logout();
-    res.clearCookie('accessToken');
-    res.redirect('/');
-});
-
-app.get('/api/me',
-    passport.authenticate('bearer', {session: false}),
-    (req, res) => res.json({
-        googleId: req.user.googleId
-    })
-);
-
-app.get('/api/questions',
-    passport.authenticate('bearer', {session: false}),
-    (req, res) => res.json(['Question 1', 'Question 2'])
-);
+// passport.use(
+//     new GoogleStrategy({
+//         clientID:  secret.CLIENT_ID,
+//         clientSecret: secret.CLIENT_SECRET,
+//         callbackURL: `/api/auth/google/callback`
+//     },
+//     (accessToken, refreshToken, profile, cb) => {
+//         // Job 1: Set up Mongo/Mongoose, create a User model which store the
+//         // google id, and the access token
+//         // Job 2: Update this callback to either update or create the user
+//         // so it contains the correct access token
+//
+//         const searchQuery = {
+//             googleId: profile.id
+//         };
+//
+//         const user = database[accessToken] = {
+//             googleId: profile.id,
+//             accessToken: accessToken
+//         };
+//
+//         const updates = {
+//             name: profile.displayName,
+//             accessToken: accessToken,
+//             googleId: profile.id
+//         };
+//
+//         const options = {
+//             upsert: true
+//         };
+//
+//         User.findOneAndUpdate(searchQuery, updates, options, (err, user) => {
+//             if (err) {
+//                 return cb(err);
+//             }
+//             else {
+//                 return cb(null, user);
+//             }
+//         })
+//     }
+// ));
+//
+// passport.use(
+//     new BearerStrategy(
+//         (token, done) => {
+//             // Job 3: Update this callback to try to find a user with a
+//             // matching access token.  If they exist, let em in, if not,
+//             // don't.
+//             if (!(token in database)) {
+//                 return done(null, false);
+//             }
+//             return done(null, database[token]);
+//         }
+//     )
+// );
+//
+// app.get('/api/auth/google',
+//     passport.authenticate('google', {scope: ['profile']}));
+//
+// app.get('/api/auth/google/callback',
+//     passport.authenticate('google', {
+//         failureRedirect: '/',
+//         session: false
+//     }),
+//     (req, res) => {
+//         res.cookie('accessToken', req.user.accessToken, {expires: 0});
+//         res.redirect('/');
+//     }
+// );
+//
+// app.get('/api/auth/logout', (req, res) => {
+//     req.logout();
+//     res.clearCookie('accessToken');
+//     res.redirect('/');
+// });
+//
+// app.get('/api/me',
+//     passport.authenticate('bearer', {session: false}),
+//     (req, res) => res.json({
+//         googleId: req.user.googleId
+//     })
+// );
+//
+// app.get('/api/questions',
+//     passport.authenticate('bearer', {session: false}),
+//     (req, res) => res.json(['Question 1', 'Question 2'])
+// );
 
 // Serve the built client
 app.use(express.static(path.resolve(__dirname, '../client/build')));
@@ -156,5 +158,5 @@ if (require.main === module) {
 }
 
 module.exports = {
-    app, runServer, closeServer
+    app, runServer, closeServer, secret
 };
