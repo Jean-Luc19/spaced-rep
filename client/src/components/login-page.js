b6e4563700dde48ffde00c4488d7b500e8890ed3
James Guthrie <jamesrguthrie@gmail.com>
2017-04-20 10:24:28 -0400

    working with login css

--- client/src/components/login-page.js
+++ client/src/components/login-page.js
@@ -3,7 +3,11 @@ import React from 'react';
 export default function LoginPage() {
     return (
         <div className="login-page">
-            <a href={'/api/auth/google'}><button className="loginBtn loginBtn--google">Login with Google</button></a>
+            <h1 className="title">Dolthraki-X</h1>
+            <h2 className="page-info">Learn the language Dothraki through spaced-repitition so that you may one day claim the iron throne.</h2>
+            <div className="btn-container">
+                <a href={'/api/auth/google'}><button className="login-btn login-btn-google">Login with Google</button></a>
+            </div>
         </div>
     );
 }
