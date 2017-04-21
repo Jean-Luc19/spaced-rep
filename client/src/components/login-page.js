0ba3eeaf035f038a70f2e091b88ea8052c2f0b0e
James Guthrie <jamesrguthrie@gmail.com>
2017-04-21 11:54:38 -0400

    added media query for mobile

--- client/src/components/login-page.js
+++ client/src/components/login-page.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import QuestionPageTitle from './question-page-title';
 
 export default function LoginPage() {
     return (
         <div className="login-page">
-            <h1 className="title">Dolthraki-X</h1>
+            <h1 className="title">Dothraki-X</h1>
             <h2 className="page-info">Learn the language Dothraki through spaced-repitition so that you may one day claim the iron throne.</h2>
             <div className="btn-container">
                 <a href={'/api/auth/google'}><button className="login-btn login-btn-google">Login with Google</button></a>
