41064e0210c88573c8915670ef35fe5786131c3e
James Guthrie <jamesrguthrie@gmail.com>
2017-04-21 15:42:17 -0400

    added new font to login-page title

--- client/src/components/login-page.js
+++ client/src/components/login-page.js
@@ -5,7 +5,7 @@ export default function LoginPage() {
     return (
         <div className="login-page">
             <h1 className="title">Dothraki-X</h1>
-            <h2 className="page-info">Learn the language Dothraki through spaced-repitition so that you may one day claim the iron throne.</h2>
+            <h2 className="page-info">Learn the language Dothraki through spaced-repetition so that you may one day claim the iron throne.</h2>
             <div className="btn-container">
                 <a href={'/api/auth/google'}><button className="login-btn login-btn-google">Login with Google</button></a>
             </div>
