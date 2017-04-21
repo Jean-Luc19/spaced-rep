e6ebe9708c807b19b24a1b5aa283615ba9b4d3bf
James Guthrie <jamesrguthrie@gmail.com>
2017-04-20 21:44:18 -0400

    added css styling and message popup to question-correct-false card

--- client/src/components/question-correct-false.js
+++ client/src/components/question-correct-false.js
@@ -27,12 +27,15 @@ class WrongCard extends React.Component {
         const userAnswer = this.props.userAnswer;
         const question = this.props.question;
         const style = this.state.correct ? {color: 'red'} : {color: 'steelblue'};
+        const message = this.state.correct ? "" : "hidden-message";
 
         return (
             <div className="flash-card result-card-wrong">
-                <h2>The Answer Was: {answer.toLowerCase()}</h2>
+                <h2>The Word Was</h2>
                 <h3 className='feedback' id='result-question' style={style} >{question}</h3>
+                <h2>The Answer Was</h2>
                 <h3 className='feedback' style={style} >{userAnswer.toLowerCase()}</h3>
+                <h4 className={message}>Darn! you got it wrong.</h4>
                 <button onClick={() => this.props.dispatch(actions.nextQuestion())}>Next</button>
             </div>
         );
